0415066045b7e0f9b30662081b071be9cc03a4ed
Armin D <a.delalic@devla.dev>
2021-04-30 21:39:31 +0200

    Add Dashboard UI components and changes

---
+++ src/components/company/CandidateList.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="candidate-list">
+    <div class="title text-capitalize font-weight-bold"
+         :class="`${list.type}`">
+      {{list.type}}
+    </div>
+    <div class="list pt-6">
+      <div class="item mb-3" v-for="(item, i) in list.items" :key="i">
+        <CandidateCard :candidate="item"
+                       :type="list.type"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import CandidateCard from '@/components/company/CandidateCard';
+
+export default {
+  name: 'CandidateList',
+
+  components: {
+    CandidateCard,
+  },
+
+  props: {
+    list: {
+      type: Object,
+    },
+  },
+};
+
+</script>
+
+<style lang="scss" scoped>
+  .request {
+    color: $status-request;
+  }
+  .negotiation {
+    color: $status-negotiation;
+  }
+  .call {
+    color: $status-call;
+  }
+</style>
--- src/views/company/Dashboard.vue
+++ src/views/company/Dashboard.vue
@@ -1,13 +1,134 @@
 <template>
-  <div>
-    Company Dashboard Page
-  </div>
+  <v-container>
+    <v-row class="heading ma-0 mb-10 flex-column">
+      <div>Hello,</div>
+      <div class="text-color-primary-blue-dark font-weight-bold">
+        John Doe
+      </div>
+    </v-row>
+    <v-row class="mb-16">
+      <v-col cols="5">
+        <div class="hd pb-7">Profile Views</div>
+        <v-card flat
+                height="240px">
+        </v-card>
+      </v-col>
+      <v-col cols="7">
+        <div class="hd pb-7">People Reach</div>
+        <v-card flat 
+                height="240px">
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col v-for="(list, i) in candidates" :key="i">
+        <CandidateList :list="list"/>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import CandidateList from '@/components/company/CandidateList';
 
 export default {
   name: 'CompanyDashboard',
+
+  components: {
+    CandidateList
+  },
+
+  data() {
+    return {
+      candidates: {
+        0: {
+          type: 'request',
+          items: [
+            {
+              name: 'Victoria Anderson',
+              location: 'Berlin',
+              job: 'UI/UX Designer',
+              note: false,
+            },
+            {
+              name: 'Victoria Anderson',
+              location: 'Berlin',
+              job: 'UI/UX Designer',
+              note: true,
+            },
+            {
+              name: 'Victoria Anderson',
+              location: 'Berlin',
+              job: 'UI/UX Designer',
+              note: false,
+            },
+          ]
+        },
+        1: {
+          type: 'negotiation',
+          items: [
+            {
+              name: 'Victoria Anderson',
+              location: 'Berlin',
+              job: 'UI/UX Designer',
+              note: false,
+            },
+            {
+              name: 'Victoria Anderson',
+              location: 'Berlin',
+              job: 'UI/UX Designer',
+              note: false,
+            },
+            {
+              name: 'Victoria Anderson',
+              location: 'Berlin',
+              job: 'UI/UX Designer',
+              note: true,
+            },
+          ]
+        },
+        2: {
+          type: 'call',
+          items: [
+            {
+              name: 'Victoria Anderson',
+              location: 'Berlin',
+              job: 'UI/UX Designer',
+              note: false,
+            },
+            {
+              name: 'Victoria Anderson',
+              location: 'Berlin',
+              job: 'UI/UX Designer',
+              note: true,
+            },
+            {
+              name: 'Victoria Anderson',
+              location: 'Berlin',
+              job: 'UI/UX Designer',
+              note: false,
+            },
+          ]
+        }
+      }
+    };
+  }
 };
 
 </script>
+
+<style scoped>
+  .hd {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: normal;
+    font-size: 20px;
+    color: #82858C;
+  }
+
+  .heading {
+    font-family: 'Open Sans';
+    font-weight: 400;
+    font-size: 20px;
+  }
+</style>
