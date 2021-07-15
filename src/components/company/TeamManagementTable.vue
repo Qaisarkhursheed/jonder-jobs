77ba71b5779e0cbfcd74165d7136e59106f97150
Armin D <a.delalic@devla.dev>
2021-07-15 02:14:35 +0200

    Add team management page components

---
+++ src/components/company/TeamManagementTable.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="team-management-table">
+    <v-data-table
+      v-model="selected"
+      :headers="headers"
+      :items="dummy"
+      :single-select="true"
+      item-key="name"
+      show-select
+      flat
+      class="table"
+    >
+      <template v-slot:[`item.actions`]="{ item }">
+        <v-btn
+          @click="removeUser(item)"
+          color="#F12727"
+          class="full-w font-weight-medium"
+          style="color: #fff"
+        >
+          Remove
+        </v-btn>
+      </template>
+    </v-data-table>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'TeamManagementTable',
+
+  data() {
+    return {
+      selected: null,
+      headers: [
+        {
+          text: 'User',
+          value: 'user',
+          width: '25%'
+        },
+        {
+          text: 'Email',
+          value: 'email',
+          width: '25%'
+        },
+        {
+          text: 'Status',
+          value: 'status',
+          width: '15%'
+        },
+        {
+          text: 'Invited on',
+          value: 'invited_on',
+          width: '15%'
+        },
+         { 
+           text: '', 
+           value: 'actions',
+           width: '10%'
+         }
+      ],
+      dummy:[
+        {
+          id: 1,
+          user: "Max Mustermann",
+          email: "marko@plutus.eu",
+          status: "Active",
+          invited_on: "Jun 14, 12:33 PM"
+        },
+        {
+          id: 2,
+          user: "Max Mustermann",
+          email: "marko@plutus.eu",
+          status: "Active",
+          invited_on: "Jun 14, 12:33 PM"
+        },
+        {
+          id: 3,
+          user: "Max Mustermann",
+          email: "marko@plutus.eu",
+          status: "Active",
+          invited_on: "Jun 14, 12:33 PM"
+        }
+      ]
+    };
+  },
+
+  methods: {
+    removeUser() {}
+  }
+};
+</script>
+<style lang="scss" scoped>
+.team-management-table {
+  .table {
+    border: 1px solid #E6E7E9;
+  }
+}
+</style>
--- src/views/company/TeamManagement.vue
+++ src/views/company/TeamManagement.vue
@@ -1,9 +1,56 @@
 <template>
-  <div>CompanyTeamManagement</div>
+  <div class="team-management">
+    <v-row class="heading mb-5">
+      <v-col cols="9">
+        <div class="title">Team Management</div>
+        <div class="subtitle">Explanation what is team managment</div>
+      </v-col>
+      <v-col cols="3">
+        <v-btn
+          color="primary"
+          height="48"
+          class="full-w font-weight-medium "
+        >
+          Invite new user
+        </v-btn>
+      </v-col>
+    </v-row>
+    <div class="table">
+      <TeamManagementTable />
+    </div>
+  </div>
 </template>
 
 <script>
+
+import TeamManagementTable from '@/components/company/TeamManagementTable'
+
 export default {
-  name: 'CompanyTeamManagement'
+  name: 'CompanyTeamManagement',
+
+  components: {
+    TeamManagementTable
+  },
+
 };
 </script>
+
+<style lang="scss" scoped>
+
+.heading {
+  font-weight: 600;
+  font-size: 32px;
+  color: #222222;
+
+  .title {
+    font-weight: 600;
+    font-size: 32px !important;
+    color: #222222;
+  }
+  .subtitle {
+    font-weight: normal;
+    font-size: 18px;
+    color: #7A7A7A;
+  }
+}
+</style>
