04e745ff1b8157ce224f3d4c1c5c92716147b7b9
muca <turaliccmustafa@gmail.com>
2021-08-07 11:31:33 +0200

    stripe

--- src/views/dashboard/UpgradePlanModal.vue
+++ src/views/dashboard/UpgradePlanModal.vue
@@ -1,106 +1,112 @@
 <template>
-  <v-dialog class="dialog"
-            v-model="active"
-            @click:outside="close('abort')"
-            persistent
-            width="750px"
-            max-width="750px">
+  <v-dialog
+    class="dialog"
+    v-model="active"
+    @click:outside="close('abort')"
+    persistent
+    width="750px"
+    max-width="750px"
+  >
     <v-card flat class="rounded-lg wrap upgrade-dialog">
-        <div class="modal-title">
-            Choose upgrade plan
-        </div>
+      <div class="modal-title">
+        Choose upgrade plan
+      </div>
 
-        <div class="options">
-            <v-btn  @click="form.active_plan = '35'"
-                     v-bind:color="form.active_plan == '35' ? 'primary' : ''"
-                     height="104"
-                     class="upgrade-option">
-               
-                
-                <v-img class="upgrade-icon"
-                :src="require('@/assets/icons/top-rated.svg')"></v-img>
+      <div class="options">
+        <v-btn
+          @click="form.active_plan = '35'"
+          v-bind:color="form.active_plan == '35' ? 'primary' : ''"
+          height="104"
+          class="upgrade-option"
+          href="https://buy.stripe.com/test_8wM6s04n502G76MeUV"
+        >
+          <v-img
+            class="upgrade-icon"
+            :src="require('@/assets/icons/top-rated.svg')"
+          ></v-img>
 
-                <div>
-                    <span class="upgrade-title">
-                        Your account is higlighted in company search
-                    </span>
-                    <p class="upgrade-text">
-                        Contrary to popular belief, Lorem Ipsum is not simply random text.
-                    </p>
-                    <span class="updgrade-price">35€</span>
-                </div>
-            </v-btn>
+          <div>
+            <span class="upgrade-title">
+              Your account is higlighted in company search
+            </span>
+            <p class="upgrade-text">
+              Contrary to popular belief, Lorem Ipsum is not simply random text.
+            </p>
+            <span class="updgrade-price">35€</span>
+          </div>
+        </v-btn>
 
-            <v-btn  @click="form.active_plan = '10'" 
-                     v-bind:color="form.active_plan == '10' ? 'primary' : ''"
-                    height="104" 
-                    class="upgrade-option">
-                
-                
-                <v-img class="upgrade-icon"
-                :src="require('@/assets/icons/medal.svg')"></v-img>
+        <v-btn
+          @click="form.active_plan = '10'"
+          v-bind:color="form.active_plan == '10' ? 'primary' : ''"
+          height="104"
+          class="upgrade-option"
+          href="https://buy.stripe.com/test_eVa03CdXF9DgfDi8ww"
+        >
+          <v-img
+            class="upgrade-icon"
+            :src="require('@/assets/icons/medal.svg')"
+          ></v-img>
 
-                <div>
-                    <span class="upgrade-title">
-                        Your account is higlighted in company search
-                    </span>
-                    <p class="upgrade-text">
-                        Contrary to popular belief, Lorem Ipsum is not simply random text.
-                    </p>
-                    <span class="updgrade-price">10€</span>
-                </div>
-            </v-btn>
-        </div>
-
-        
+          <div>
+            <span class="upgrade-title">
+              Your account is higlighted in company search
+            </span>
+            <p class="upgrade-text">
+              Contrary to popular belief, Lorem Ipsum is not simply random text.
+            </p>
+            <span class="updgrade-price">10€</span>
+          </div>
+        </v-btn>
+      </div>
 
       <div class="buttons">
-          <v-btn
-              @click="close"
-              height="56"
-              class="mt-16 font-weight-medium upgrade-btn"
-              style="margin-right: 20px;"
-          >
-              {{ $t('general.cancel') }}
-          </v-btn>
-          <v-btn
-              @click="save"
-              color="primary"
-              height="56"
-              width = "178"
-              class="mt-16 font-weight-medium upgrade-btn"
-          >
-              Next
-          </v-btn>
+        <v-btn
+          @click="close"
+          height="56"
+          class="mt-16 font-weight-medium upgrade-btn"
+          style="margin-right: 20px;"
+        >
+          {{ $t("general.cancel") }}
+        </v-btn>
+        <v-btn
+          @click="save"
+          color="primary"
+          height="56"
+          width="178"
+          class="mt-16 font-weight-medium upgrade-btn"
+        >
+          Next
+        </v-btn>
       </div>
     </v-card>
   </v-dialog>
 </template>
 
 <script>
-import store from '@/store';
+import store from "@/store";
 
 export default {
-  name: 'UpgradePlanModal',
+  name: "UpgradePlanModal",
 
   props: {
     active: {
       type: Boolean,
-      default: false
+      default: false,
     },
     type: {
       type: String,
-      default: 'ok'
+      default: "ok",
     },
     edit: {
-      type: [Object, Boolean]
-    }
+      type: [Object, Boolean],
+    },
   },
   data() {
     return {
       form: {
-        active_plan: '',
-      }
+        active_plan: "",
+      },
     };
   },
   created() {
@@ -110,62 +116,62 @@ export default {
   },
   methods: {
     close(type) {
-      this.$emit('close', type);
+      this.$emit("close", type);
     },
     save() {
       if (this.edit) {
-        store.dispatch('user/updateUser', {
+        store.dispatch("user/updateUser", {
           id: this.edit.id,
           payload: this.form,
         });
       } else {
-        console.log()
+        console.log();
       }
-      this.$emit('close', 1);
+      this.$emit("close", 1);
     },
     populate() {
       this.form.active_plan = this.edit.active_plan;
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .dialog {
-   font-family: $inter !important;
+  font-family: $inter !important;
 }
 
 .modal-title {
-    padding: 40px;
-    font-size: 20px;
-    font-weight: 600;
-    line-height: 34px;
+  padding: 40px;
+  font-size: 20px;
+  font-weight: 600;
+  line-height: 34px;
 }
 .options {
-    margin: 0 40px;
+  margin: 0 40px;
 
-    .upgrade-option {
-        background: white;
-        border: 1px solid #E9E9E9;
-        margin-bottom: 24px;
-        outline: none;
-        flex: 0 0 100%;
-        box-shadow: none;
-        text-align: left;
-    }
+  .upgrade-option {
+    background: white;
+    border: 1px solid #e9e9e9;
+    margin-bottom: 24px;
+    outline: none;
+    flex: 0 0 100%;
+    box-shadow: none;
+    text-align: left;
+  }
 
-    .upgrade-text {
-        font-size: 11px;
-        line-height: 12px;
-        word-break: break-all;
-        padding-top: 9px;
-    }
+  .upgrade-text {
+    font-size: 11px;
+    line-height: 12px;
+    word-break: break-all;
+    padding-top: 9px;
+  }
 }
 
 .buttons {
-    display: flex;
-    width: 100%;
-    justify-content: flex-end;
-    padding: 40px;
+  display: flex;
+  width: 100%;
+  justify-content: flex-end;
+  padding: 40px;
 }
 </style>
