c83422bf21964d4d2cf086be174dcb2e5819fa2e
Teo <juricic.teo@nsoft.com>
2021-08-29 23:12:03 +0200

    Remove store vertification time on auth reg

--- src/components/auth/AuthRegister.vue
+++ src/components/auth/AuthRegister.vue
@@ -108,7 +108,7 @@
         placeholder="Passwort erneut eingeben"
         :rules="[
           validations.required,
-          validations.same('Passwort', formData.password),
+          validations.same('Passwort', formData.password)
         ]"
         :type="showPassConfirm ? 'text' : 'password'"
         dense
@@ -198,7 +198,7 @@ import JonderTitle from "../parts/JonderTitle.vue";
 
 export default {
   components: {
-    JonderTitle,
+    JonderTitle
   },
   data() {
     return {
@@ -212,14 +212,14 @@ export default {
         // show_name: false,
         // show_location: false,
         role: "Jobseeker",
-        accept_policy: false,
+        accept_policy: false
       },
       profile_img: false,
       showPass: false,
       showPassConfirm: false,
       formLoading: false,
       formResponse: {},
-      formValid: false,
+      formValid: false
     };
   },
   methods: {
@@ -232,17 +232,16 @@ export default {
       this.$store
         .dispatch("auth/register", this.formData)
         .then(() => {
-          const time = new Date().getTime().toString();
           localStorage.setItem("user-email", this.formData.email);
           this.$router.replace({ name: "RegisterVerifyEmail" });
         })
-        .catch((err) => {
+        .catch(err => {
           this.formResponse = err.data;
         })
         .finally(() => {
           this.formLoading = false;
         });
-    },
+    }
   },
   computed: {
     avatar_img() {
@@ -250,8 +249,8 @@ export default {
         return URL.createObjectURL(this.profile_img);
       }
       return false;
-    },
-  },
+    }
+  }
 };
 </script>
 
