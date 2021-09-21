658bdc3e66830466681afc8d0a989cc7ae2a22ae
Teo <juricic.teo@nsoft.com>
2021-09-21 12:06:31 +0200

    Update on langugae

--- src/components/LanguageDropdown.vue
+++ src/components/LanguageDropdown.vue
@@ -1,28 +1,30 @@
 <template>
-  <v-select
-    :value="$store.getters['user/user'].locale"
-    :items="items"
-    @change="changeLanguage"
-    outlined
-    hide-details
-  >
-    <template v-slot:selection="{ item }">
-      <img
-        width="20"
-        class="rounded-circle mr-3"
-        :src="require('@/assets/flags/' + item.value + '.svg')"
-      />
-      {{ $t(item.text) }}
-    </template>
-    <template v-slot:item="{ item }">
-      <img
-        width="20"
-        class="rounded-circle mr-3"
-        :src="require('@/assets/flags/' + item.value + '.svg')"
-      />
-      {{ $t(item.text) }}
-    </template>
-  </v-select>
+  <div class="language-toggle">
+    <v-select
+        :value="getLanguage"
+        :items="items"
+        @change="changeLanguage"
+        outlined
+        hide-details
+    >
+      <template v-slot:selection="{ item }">
+        <img
+            width="20"
+            class="rounded-circle mr-3"
+            :src="require('@/assets/flags/' + item.value + '.svg')"
+        />
+        {{ $t(item.value) }}
+      </template>
+      <template v-slot:item="{ item }">
+        <img
+            width="20"
+            class="rounded-circle mr-3"
+            :src="require('@/assets/flags/' + item.value + '.svg')"
+        />
+        {{ $t(item.text) }}
+      </template>
+    </v-select>
+  </div>
 </template>
 
 <script>
@@ -45,16 +47,29 @@ export default {
       ]
     };
   },
-
+  computed: {
+    getLanguage() {
+      return this.$store.getters["user/user"]
+        ? this.$store.getters["user/user"].locale
+        : localStorage.getItem("lang") || "de";
+    }
+  },
   methods: {
     changeLanguage(locale) {
-      this.$store.dispatch("user/setLocale", locale).then(() => {
-        this.$i18n.locale = locale;
-        localStorage.setItem("lang", locale);
-      });
+      if (this.$store.getters["user/user"]) {
+        this.$store.dispatch("user/setLocale", locale);
+      }
+      this.$i18n.locale = locale;
+      localStorage.setItem("lang", locale);
     }
-  }
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.language-toggle {
+  display: inline-block;
+  padding-left: 20px;
+  width: 120px;
+}
+</style>
