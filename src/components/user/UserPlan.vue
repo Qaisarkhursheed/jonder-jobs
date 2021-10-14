300a4f5bc3ae2ce32273ad8ed701531c61841d4a
i.tricic <i.tricic@devla.dev>
2021-10-14 18:06:42 +0200

    add no active plan text

--- src/components/user/UserPlan.vue
+++ src/components/user/UserPlan.vue
@@ -3,7 +3,22 @@
     <h2>{{ $t("subscriptionPlan") }}</h2>
     <p>{{ $t("yourCurrentPlanDescription") }}</p>
 
+    <template v-if="!userPlan || !userPlan[0]">
+      <div>
+        {{ $t("noActivePlan") }}
+      </div>
+
+      <router-link
+        v-if="$route.name != 'EmployerPackagesPricing'"
+        :to="{ name: 'EmployerPackagesPricing' }"
+        style="text-decoration: none"
+      >
+        {{ $t("upgradeAccount") }}
+      </router-link>
+    </template>
+
     <UserPlanDescription
+      v-else
       :plan="userPlan[0]"
       :border-plan="borderPlan"
       :payment-info="paymentInfo"
